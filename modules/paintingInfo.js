71f05dff0519dd79851f93de416a12515caa6621
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2024-05-01 23:36:27 +0200

    fix : displaypaintinfo

--- modules/paintingInfo.js
+++ modules/paintingInfo.js
@@ -1,19 +1,15 @@
-// Display painting info in the DOM
 export const displayPaintingInfo = (info) => {
-  const infoElement = document.getElementById("painting-info"); // Get the reference
-
-  // Set the html content inside info element
+  const infoElement = document.getElementById("painting-info")
   infoElement.innerHTML = `
     <h3>${info.title}</h3>
     <p>Artist: ${info.artist}</p>
     <p>Description: ${info.description}</p>
     <p>Year: ${info.year}</p>
-  `;
-  infoElement.classList.add("show"); // Add the 'show' class
-};
+  `
+  infoElement.classList.add("show")
+}
 
-// Hide painting info in the DOM
 export const hidePaintingInfo = () => {
-  const infoElement = document.getElementById("painting-info"); // Get the reference
-  infoElement.classList.remove("show"); // Remove the 'show' class
-};
+  const infoElement = document.getElementById("painting-info")
+  infoElement.classList.remove("show")
+}
